e418d98874350735d1e8f7c09101af0159df3893
Ufuk Arikan <ufuk.arikan@adnovum.ch>
2020-06-10 14:30:32 +0200

    1.3 refactor

--- part1/1.1/src/index.js
+++ part1/1.1/src/index.js
@@ -8,20 +8,23 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+    console.log(props)
     return (
         <>
-            <Part part={props.part} exercise={props.exercise} />
+            <Part part={props.part} exercise={props.exercise}/>
         </>
     )
 }
 
 const Part = (props) => {
+    console.log(props)
     return (
         <p>{props.part} {props.exercise}</p>
     )
 }
 
 const Total = (props) => {
+    console.log(props)
     return (
         <p>Number of exercises {props.total}</p>
     )
@@ -29,20 +32,26 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
-            <Header course={course} />
-            <Content part={part1} exercise={exercises1} />
-            <Content part={part2} exercise={exercises2} />
-            <Content part={part3} exercise={exercises2} />
-            <Total total={exercises1 + exercises2 + exercises3} />
+            <Header course={course}/>
+            <Content part={part1.name} exercise={part1.exercises}/>
+            <Content part={part2.name} exercise={part2.exercises}/>
+            <Content part={part3.name} exercise={part3.exercises}/>
+            <Total total={part1.exercises + part2.exercises + part3.exercises}/>
         </div>
     )
 }
