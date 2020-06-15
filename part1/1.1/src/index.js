213855db4b9610f25f47e7aeab1dc257d2d55254
BlueScoreMan <face8105@gmail.com>
2020-06-15 17:24:27 +0200

    add hooks and handlers
     Date:      Fri Jun 12 14:24:27 2020 +0200

--- part1/1.1/src/index.js
+++ part1/1.1/src/index.js
@@ -1,26 +1,52 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-// Destructuring here
-// And simplifying return since it has only one return argument
-const Display = ({counter}) => <div>{counter}</div>
+const App = (props) => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
-// Same here tbh
-const Buttons = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
 
-const App = () => {
-    const [counter, setCounter] = useState(0)
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
 
-    const increaseByOne = () => setCounter(counter + 1)
-    const decreaseByOne = () => setCounter(counter - 1)
-    const setToZero = () => setCounter(0)
+    const History = ({allClicks}) => {
+        if (allClicks.length === 0) {
+            return (
+                <div>
+                    <p>press buttons to unlock full potential of the application</p>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <p> button press history: {allClicks.join(" ")}</p>
+            </div>
+        )
+    }
+
+    const Button = (props) => {
+        return (
+            <button onClick={props.onClick}>{props.text}</button>
+        )
+    }
 
     return (
         <div>
-            <Display counter={counter}/>
-            <Buttons handleClick={increaseByOne} text='plus'/>
-            <Buttons handleClick={decreaseByOne} text='subtract'/>
-            <Buttons handleClick={setToZero} text='clear'/>
+            <div>
+                {left}
+                <Button onClick={handleLeftClick} text={"left"}/>
+                <Button onClick={handleRightClick} text={"right"}/>
+                {right}
+                <History allClicks={allClicks}/>
+            </div>
         </div>
     )
 }
