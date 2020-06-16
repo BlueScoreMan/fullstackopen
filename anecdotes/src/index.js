7b849ddc8a8617dd629b817c2bc0d710c31883cc
BlueScoreMan <face8105@gmail.com>
2020-06-16 18:37:12 +0200

    anecodtes 1.13* commit

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -7,15 +7,24 @@ const getRandomInt = () => {
 
 const App = (props) => {
     const [selected, setSelected] = useState(0)
+    const [points, setPoints] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0})
 
     const handleQuote = () => {
         setSelected(getRandomInt())
     }
 
+    const handleVote = () => {
+        const copy = {...points}
+        copy[selected] += 1
+        setPoints(copy)
+    }
+
     return (
         <div>
             <button onClick={handleQuote}>next quote</button>
+            <button onClick={handleVote}>vote</button>
             {props.anecdotes[selected]}
+            has {points[selected]} votes
         </div>
     )
 }
